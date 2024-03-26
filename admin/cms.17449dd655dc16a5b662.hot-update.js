9c8dd745600f58ac49861c5bbe4daa2705ba9639
Austin Liu <austinliu279@gmail.com>
2024-03-26 07:09:56 -0700

    Updates

---
+++ admin/cms.17449dd655dc16a5b662.hot-update.js
@@ -0,0 +1,11 @@
+"use strict";
+self["webpackHotUpdategatsby_starter_decap_cms"]("cms",{},
+/******/ function(__webpack_require__) { // webpackRuntimeModules
+/******/ /* webpack/runtime/getFullHash */
+/******/ !function() {
+/******/ 	__webpack_require__.h = function() { return "58fdf969064f08f28732"; }
+/******/ }();
+/******/ 
+/******/ }
+);
+//# sourceMappingURL=cms.17449dd655dc16a5b662.hot-update.js.map
---
+++ admin/cms.5fb67e07786f4625b40c.hot-update.js
@@ -0,0 +1,53 @@
+"use strict";
+self["webpackHotUpdategatsby_starter_decap_cms"]("cms",{
+
+/***/ "./.cache/caches/gatsby-plugin-image/2369379629.json":
+/*!***********************************************************!*\
+  !*** ./.cache/caches/gatsby-plugin-image/2369379629.json ***!
+  \***********************************************************/
+/***/ (function(module) {
+
+module.exports = /*#__PURE__*/JSON.parse('{"layout":"constrained","backgroundColor":"#080808","images":{"fallback":{"src":"/pcc-nam-website-ui/static/dbce5b508efe45797bf7c8980efc9961/fdd84/contentful-logo.svg","srcSet":"/pcc-nam-website-ui/static/dbce5b508efe45797bf7c8980efc9961/04410/contentful-logo.svg 188w,\\n/pcc-nam-website-ui/static/dbce5b508efe45797bf7c8980efc9961/a161f/contentful-logo.svg 375w,\\n/pcc-nam-website-ui/static/dbce5b508efe45797bf7c8980efc9961/fdd84/contentful-logo.svg 750w","sizes":"(min-width: 750px) 750px, 100vw"},"sources":[{"srcSet":"/pcc-nam-website-ui/static/dbce5b508efe45797bf7c8980efc9961/42f3c/contentful-logo.webp 188w,\\n/pcc-nam-website-ui/static/dbce5b508efe45797bf7c8980efc9961/4d73f/contentful-logo.webp 375w,\\n/pcc-nam-website-ui/static/dbce5b508efe45797bf7c8980efc9961/6dc4f/contentful-logo.webp 750w","type":"image/webp","sizes":"(min-width: 750px) 750px, 100vw"}]},"width":750,"height":153}');
+
+/***/ }),
+
+/***/ "./.cache/caches/gatsby-plugin-image/2922020879.json":
+/*!***********************************************************!*\
+  !*** ./.cache/caches/gatsby-plugin-image/2922020879.json ***!
+  \***********************************************************/
+/***/ (function(module) {
+
+module.exports = /*#__PURE__*/JSON.parse('{"layout":"fullWidth","placeholder":{"fallback":"data:image/jpeg;base64,/9j/2wBDABALDA4MChAODQ4SERATGCgaGBYWGDEjJR0oOjM9PDkzODdASFxOQERXRTc4UG1RV19iZ2hnPk1xeXBkeFxlZ2P/2wBDARESEhgVGC8aGi9jQjhCY2NjY2NjY2NjY2NjY2NjY2NjY2NjY2NjY2NjY2NjY2NjY2NjY2NjY2NjY2NjY2NjY2P/wgARCAAOABQDASIAAhEBAxEB/8QAGQAAAgMBAAAAAAAAAAAAAAAAAAMBAgQF/8QAFAEBAAAAAAAAAAAAAAAAAAAAAP/aAAwDAQACEAMQAAAB4LKNM5If/8QAGRABAQADAQAAAAAAAAAAAAAAAQACAxES/9oACAEBAAEFAg7Za/JCk7ckv//EABQRAQAAAAAAAAAAAAAAAAAAABD/2gAIAQMBAT8BP//EABQRAQAAAAAAAAAAAAAAAAAAABD/2gAIAQIBAT8BP//EABcQAAMBAAAAAAAAAAAAAAAAAAAQEQH/2gAIAQEABj8CLccX/8QAGRABAAMBAQAAAAAAAAAAAAAAAQAQESFh/9oACAEBAAE/IWWROg87TujNia//2gAMAwEAAgADAAAAEEsP/8QAFhEAAwAAAAAAAAAAAAAAAAAAARBh/9oACAEDAQE/EBV//8QAFREBAQAAAAAAAAAAAAAAAAAAEEH/2gAIAQIBAT8Qh//EABwQAQACAQUAAAAAAAAAAAAAAAEAESEQMUFRkf/aAAgBAQABPxAWBpasFqPaU4AfENBChOouwHeo5n//2Q=="},"images":{"fallback":{"src":"/pcc-nam-website-ui/static/2247c39f145df7505f72ce04ceaeef39/71357/macbook-color.jpg","srcSet":"/pcc-nam-website-ui/static/2247c39f145df7505f72ce04ceaeef39/982db/macbook-color.jpg 750w,\\n/pcc-nam-website-ui/static/2247c39f145df7505f72ce04ceaeef39/e4f86/macbook-color.jpg 1080w,\\n/pcc-nam-website-ui/static/2247c39f145df7505f72ce04ceaeef39/c255e/macbook-color.jpg 1366w,\\n/pcc-nam-website-ui/static/2247c39f145df7505f72ce04ceaeef39/71357/macbook-color.jpg 1920w","sizes":"100vw"},"sources":[{"srcSet":"/pcc-nam-website-ui/static/2247c39f145df7505f72ce04ceaeef39/b99ea/macbook-color.webp 750w,\\n/pcc-nam-website-ui/static/2247c39f145df7505f72ce04ceaeef39/acf6c/macbook-color.webp 1080w,\\n/pcc-nam-website-ui/static/2247c39f145df7505f72ce04ceaeef39/8245d/macbook-color.webp 1366w,\\n/pcc-nam-website-ui/static/2247c39f145df7505f72ce04ceaeef39/74526/macbook-color.webp 1920w","type":"image/webp","sizes":"100vw"}]},"width":1,"height":0.7078125}');
+
+/***/ }),
+
+/***/ "./.cache/caches/gatsby-plugin-image/3700448002.json":
+/*!***********************************************************!*\
+  !*** ./.cache/caches/gatsby-plugin-image/3700448002.json ***!
+  \***********************************************************/
+/***/ (function(module) {
+
+module.exports = /*#__PURE__*/JSON.parse('{"layout":"constrained","backgroundColor":"#080808","images":{"fallback":{"src":"/pcc-nam-website-ui/static/7c28e5696eb27affae765b6d60f487d9/b15f1/abstract-building.jpg","srcSet":"/pcc-nam-website-ui/static/7c28e5696eb27affae765b6d60f487d9/4f7c9/abstract-building.jpg 625w,\\n/pcc-nam-website-ui/static/7c28e5696eb27affae765b6d60f487d9/1b704/abstract-building.jpg 1250w,\\n/pcc-nam-website-ui/static/7c28e5696eb27affae765b6d60f487d9/b15f1/abstract-building.jpg 2500w","sizes":"(min-width: 2500px) 2500px, 100vw"},"sources":[{"srcSet":"/pcc-nam-website-ui/static/7c28e5696eb27affae765b6d60f487d9/dc158/abstract-building.webp 625w,\\n/pcc-nam-website-ui/static/7c28e5696eb27affae765b6d60f487d9/c47c6/abstract-building.webp 1250w,\\n/pcc-nam-website-ui/static/7c28e5696eb27affae765b6d60f487d9/6946a/abstract-building.webp 2500w","type":"image/webp","sizes":"(min-width: 2500px) 2500px, 100vw"}]},"width":2500,"height":1670}');
+
+/***/ }),
+
+/***/ "./.cache/caches/gatsby-plugin-image/4091309336.json":
+/*!***********************************************************!*\
+  !*** ./.cache/caches/gatsby-plugin-image/4091309336.json ***!
+  \***********************************************************/
+/***/ (function(module) {
+
+module.exports = /*#__PURE__*/JSON.parse('{"layout":"constrained","backgroundColor":"#080808","images":{"fallback":{"src":"/pcc-nam-website-ui/static/4f45c8182ffee0d628877a8209c95902/e714e/gatsby-logo.svg","srcSet":"/pcc-nam-website-ui/static/4f45c8182ffee0d628877a8209c95902/1480f/gatsby-logo.svg 298w,\\n/pcc-nam-website-ui/static/4f45c8182ffee0d628877a8209c95902/49b20/gatsby-logo.svg 596w,\\n/pcc-nam-website-ui/static/4f45c8182ffee0d628877a8209c95902/e714e/gatsby-logo.svg 1192w","sizes":"(min-width: 1192px) 1192px, 100vw"},"sources":[{"srcSet":"/pcc-nam-website-ui/static/4f45c8182ffee0d628877a8209c95902/e38e2/gatsby-logo.webp 298w,\\n/pcc-nam-website-ui/static/4f45c8182ffee0d628877a8209c95902/07ccc/gatsby-logo.webp 596w,\\n/pcc-nam-website-ui/static/4f45c8182ffee0d628877a8209c95902/9ba3b/gatsby-logo.webp 1192w","type":"image/webp","sizes":"(min-width: 1192px) 1192px, 100vw"}]},"width":1192,"height":314}');
+
+/***/ })
+
+},
+/******/ function(__webpack_require__) { // webpackRuntimeModules
+/******/ /* webpack/runtime/getFullHash */
+/******/ !function() {
+/******/ 	__webpack_require__.h = function() { return "17449dd655dc16a5b662"; }
+/******/ }();
+/******/ 
+/******/ }
+);
+//# sourceMappingURL=cms.5fb67e07786f4625b40c.hot-update.js.map
