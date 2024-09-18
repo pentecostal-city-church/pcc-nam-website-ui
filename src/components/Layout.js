bfeca80fd4a4fa1b069f5ed6656672abdd4b3f1a
Austin Liu <austinliu279@gmail.com>
2024-09-17 20:16:08 -0700

    fix ssr issue

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -5,6 +5,7 @@ import Footer from "./Footer/Footer"
 import { motion } from "framer-motion"
 import { GlobalStyle } from "../styles/GlobalStyles"
 import { withPrefix } from "gatsby";
+import window from 'global';
 
 const Layout = ({ children }) => {
   return (
@@ -16,13 +17,21 @@ const Layout = ({ children }) => {
         exit={{ opacity: 0 }}
         transition={{ duration: 0.75 }}
       >
-          {<div style={{ width: '100vw', height: '100vh', backgroundPositionX: '50%', backgroundPositionY: '50%', backgroundRepeat: 'no-repeat', backgroundAttachment: 'fixed', backgroundPosition: 'center', backgroundSize: 'cover', backgroundImage: `url("${withPrefix('/img/welcome.jpeg')}")` }}>
+          {window?.location?.pathname === '/' ? (<div style={{ width: '100vw', height: '100vh', backgroundPositionX: '50%', backgroundPositionY: '50%', backgroundRepeat: 'no-repeat', backgroundAttachment: 'fixed', backgroundPosition: 'center', backgroundSize: 'cover', backgroundImage: `url("${withPrefix('/img/welcome.jpeg')}")` }}>
             <NavModule />
             <AnimMain>
               {children}
               <Footer />
             </AnimMain>
-          </div>}
+          </div>) : (
+            <div>
+              <NavModule />
+              <AnimMain>
+                {children}
+                <Footer />
+              </AnimMain>
+            </div>
+          )}
       </motion.div>
     </>
   )
