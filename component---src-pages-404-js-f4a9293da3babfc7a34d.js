5dbc96778ee277db1951c247399c77158e5c8958
Austin Liu <austinliu279@gmail.com>
2024-09-17 22:36:45 -0700

    Updates

--- component---src-pages-404-js-f4a9293da3babfc7a34d.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkgatsby_starter_decap_cms=self.webpackChunkgatsby_starter_decap_cms||[]).push([[125],{3331:function(e,t,a){a.r(t);var s=a(6540),l=a(7650);t.default=()=>s.createElement(l.A,null,s.createElement("div",null,s.createElement("h1",null,"NOT FOUND"),s.createElement("p",null,"You just hit a route that doesn't exist... the sadness.")))}}]);
-//# sourceMappingURL=component---src-pages-404-js-f4a9293da3babfc7a34d.js.map
--- fea29d9f-68d6ed3c9531a04baf20.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkgatsby_starter_decap_cms=self.webpackChunkgatsby_starter_decap_cms||[]).push([[687],{9879:function(t,a,c){c.d(a,{mm2:function(){return r}});var e=c(2225);function r(t){return(0,e.k5)({tag:"svg",attr:{viewBox:"0 0 24 24"},child:[{tag:"path",attr:{fill:"none",d:"M0 0h24v24H0z"},child:[]},{tag:"path",attr:{d:"M20 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 4-8 5-8-5V6l8 5 8-5v2z"},child:[]}]})(t)}}}]);
-//# sourceMappingURL=fea29d9f-68d6ed3c9531a04baf20.js.map
